a5a1f1433af854de9307be859f0dad8138f66c46
shaharsol <shahar@gnift.com>
2026-05-28 15:33:13 +0300

    code finished

--- 2026-05-28/05-am-pm/frontend/src/components/layout/main/Main.tsx
+++ 2026-05-28/05-am-pm/frontend/src/components/layout/main/Main.tsx
@@ -1,9 +1,12 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import List from "../../products/list/List";
+import AddProduct from "../../products/add/AddProduct";
 
 export default function Main() {
     return (
         <Routes>
-            {/* add <Route /> as needed */}
+            <Route path="/" element={<List />} />
+            <Route path="/add-product" element={<AddProduct />} />
         </Routes>
     )
 }
