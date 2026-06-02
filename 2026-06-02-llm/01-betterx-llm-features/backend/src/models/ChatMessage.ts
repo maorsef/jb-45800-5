4520f966f9b2919fcafe8b1142e54515a1d0853b
shaharsol <shahar@gnift.com>
2026-06-02 15:42:14 +0300

    added ChatMessage model

---
+++ 2026-06-02-llm/01-betterx-llm-features/backend/src/models/ChatMessage.ts
@@ -0,0 +1,25 @@
+import { AllowNull, Column, DataType, Default, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+    underscored: true
+})
+export default class ChatMessage extends Model {
+
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id: string
+
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    chatId: string
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    role: string
+
+    @AllowNull(false)
+    @Column(DataType.TEXT)
+    content: string
+}
+
